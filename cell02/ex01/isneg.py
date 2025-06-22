74d96f84c43656236aa47c2c742fd79e1ff2cc93
68170020 <68170020@kmitl.ac.th>
2025-06-22 03:29:06 +0000

    isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+x = int(input("Enter a number: "))
+if x <0:
+    print("This number is negative.")
+if x >0:
+    print("This number is positive.")
+if x ==0:
+    print("This number is both positive and negative.")
