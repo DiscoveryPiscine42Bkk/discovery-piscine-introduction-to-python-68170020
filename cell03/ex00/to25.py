032224ae33ef7d45c691c281c9ebf9f8d4a881a4
68170020 <68170020@kmitl.ac.th>
2025-06-22 04:22:23 +0000

    to25.py

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,9 @@
+print("Enter a number less than 25")
+num = int(input())
+while num >25:
+    print("ERROR")
+    print("Enter a number less than 25")
+    num = int(input())
+  
+
+print(f"Inside the loop, my variable is {num}")
