a2b6bbc0bf3f8c1301ed668ea2b8e7263e6de3d5
68170020 <68170020@kmitl.ac.th>
2025-06-22 03:47:08 +0000

    password.py

---
+++ cell02/ex02/password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+mpassword = "Python is awesome"
+print ("Enter your password: ")
+password = input()
+if password ==mpassword:
+    print("ACCESS GRANTED")
+elif password !=mpassword:
+    print("ACCESS DENIED")
