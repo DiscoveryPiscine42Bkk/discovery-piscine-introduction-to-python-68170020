5dbe17e5df31f6df2a28a7f16e6f80687b60631c
68170020 <68170020@kmitl.ac.th>
2025-06-23 03:04:35 +0000

    whatsyourname.py

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,3 @@
 fname = input("Hey, what's your name?: ")
 lname = input("And your last name?: ")
-print (f"Well, pleased to meet you, {fname}{lname}.")
+print (f"Well, pleased to meet you, {fname} {lname}.")
