0a4b85174b04607d3b7d521323f6195771d430e5
68170020 <68170020@kmitl.ac.th>
2025-06-23 03:13:22 +0000

    mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -3,9 +3,8 @@ print("Enter the first number: ")
 n1 = int(input())
 print("Enter the second number: ")
 n2 =int(input())
-print(n1, "x",  n2)
-print(n1 * n2)
 product = n1 * n2
+print(n1, "x",  n2, "=", product)
 if product ==0:
     print("The result is positive and negative.")
 if product >0:
